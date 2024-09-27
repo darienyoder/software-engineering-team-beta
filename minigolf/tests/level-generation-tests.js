45ee6d80fa7acc356e14993ac4a9b126513154c7
darienyoder <116597751+darienyoder@users.noreply.github.com>
2024-09-27 13:34:29 -0400

    Created tests for donut shapes

--- minigolf/tests/level-generation-tests.js
+++ minigolf/tests/level-generation-tests.js
@@ -54,8 +54,8 @@ addTest('LevelGen Basic Rect', async () => {
 //
 addTest('LevelGen Shape Addition', async () => {
 
-    for (var x1 = 0; x1 < 10; x1++)
-    for (var y1 = 0; y1 < 10; y1++)
+    for (var x1 = 0; x1 < 1; x1++)
+    for (var y1 = 0; y1 < 1; y1++)
     for (var w1 = 1; w1 < 10; w1++)
     for (var h1 = 1; h1 < 10; h1++)
     for (var x2 = x1 + 1; x2 < x1 + w1; x2++)
@@ -97,14 +97,14 @@ addTest('LevelGen Shape Addition', async () => {
 //
 addTest('LevelGen Shape Subtraction', async () => {
 
-    for (var x1 = 0; x1 < 10; x1++)
-    for (var y1 = 0; y1 < 10; y1++)
-    for (var w1 = 1; w1 < 10; w1++)
-    for (var h1 = 1; h1 < 10; h1++)
+    for (var x1 = 0; x1 < 1; x1++)
+    for (var y1 = 0; y1 < 1; y1++)
+    for (var w1 = 1; w1 < 5; w1++)
+    for (var h1 = 1; h1 < 5; h1++)
     for (var x2 = x1 + 1; x2 < x1 + w1; x2++)
-    for (var y2 = y1 + 1; y2 < x2 + h2; y2++)
-    for (var w2 = 20; w2 < 30; w2++)
-    for (var h2 = 20; h2 < 30; h2++)
+    for (var y2 = y1 + 1; y2 < y1 + h1; y2++)
+    for (var w2 = 20; w2 < 21; w2++)
+    for (var h2 = 20; h2 < 21; h2++)
     {
         // Ex. ADD rect 3, 5, 7, 6; SUB rect 5, 9, 23, 28;"
         let testArea = parseAreaString(
@@ -120,6 +120,52 @@ addTest('LevelGen Shape Subtraction', async () => {
             {"X": x2, "Y": y1 + h1},
             {"X": x1, "Y": y1 + h1},
         ]], []]))
+            {
+                throw new Error("ADD rect " + String(x1) + " " + String(y1) + " " + String(w1) + " " + String(h1) + ";"
+                              + "SUB rect " + String(x2) + " " + String(y2) + " " + String(w2) + " " + String(h2) + ";");
+            }
+    }
+});
+
+
+
+// Subtracts a rectangle from inside another and
+// makes sure that the resulting polygons
+// are shaped as expected
+//
+//  +-------+
+//  | +---+ |
+//  | |   | |
+//  | +---+ |
+//  +-------+
+//
+addTest('LevelGen Donut Subtraction', async () => {
+
+    for (var x1 = 0; x1 < 10; x1++)
+    for (var y1 = 0; y1 < 10; y1++)
+    for (var w1 = 3; w1 < 10; w1++)
+    for (var h1 = 3; h1 < 10; h1++)
+    for (var x2 = x1 + 1; x2 < x1 + w1 - 2; x2++)
+    for (var y2 = y1 + 1; y2 < y1 + h1 - 2; y2++)
+    for (var w2 = 1; w2 < w1 - x2; w2++)
+    for (var h2 = 1; h2 < h1 - y2; h2++)
+    {
+        // Ex. ADD rect 3, 5, 7, 6; SUB rect 6, 8, 3, 2;"
+        let testArea = parseAreaString(
+        "ADD rect " + String(x1) + " " + String(y1) + " " + String(w1) + " " + String(h1) + ";"
+        + "SUB rect " + String(x2) + " " + String(y2) + " " + String(w2) + " " + String(h2) + ";"
+    )
+        if (!compareAreas(testArea, [[[
+            {"X": x1, "Y": y1},
+            {"X": x1 + w1, "Y": y1},
+            {"X": x1 + w1, "Y": y1 + h1},
+            {"X": x1, "Y": y1 + h1}
+        ]], [[
+            {"X": x2, "Y": y2},
+            {"X": x2 + w2, "Y": y2},
+            {"X": x2 + w2, "Y": y2 + h2},
+            {"X": x2, "Y": y2 + h2}
+        ]]]))
             {
                 throw new Error("ADD rect " + String(x1) + " " + String(y1) + " " + String(w1) + " " + String(h1) + ";"
                               + "SUB rect " + String(x2) + " " + String(y2) + " " + String(w2) + " " + String(h2) + ";");
