64299c48230e8ad23465fc40bc168e1ea0736a9c
clickyquack <159969539+clickyquack@users.noreply.github.com>
2024-11-13 13:36:33 -0500

    forgot this in the other pull request lol

--- minigolf/main.js
+++ minigolf/main.js
@@ -500,6 +500,7 @@ async function handleGamePlay() {
             gameState = 'menu'; //return to menu
             document.getElementById('startButton').style.display = 'block';  // Show the button once in menu
             document.getElementById('levelSelectButton').style.display = 'block';
+            document.getElementById('blitzModeButton').style.display = 'block';
 
         }
     }
